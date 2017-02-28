9b7fbc421e9978ff6bca05a4481da7ffd5fbad57
wefine <wang.xiaoren@zte.com.cn>
2017-02-28 17:39:57 +0800

    add webpack2 and angular, start successful.
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

---
+++ src/app/index.js
@@ -0,0 +1,16 @@
+import angular from "angular";
+
+angular.module('phonecatApp', []).controller('PhoneListController', function PhoneListController($scope) {
+    $scope.phones = [
+        {
+            name: 'Nexus S',
+            snippet: 'Fast just got faster with Nexus S.'
+        }, {
+            name: 'Motorola XOOM™ with Wi-Fi',
+            snippet: 'The Next, Next Generation tablet.'
+        }, {
+            name: 'MOTOROLA XOOM™',
+            snippet: 'The Next, Next Generation tablet.'
+        }
+    ];
+});
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// do not show deprecation warning
+process.noDeprecation = true;
+
+const appRoot = __dirname + '/src/app';
+
+module.exports = {
+    context: appRoot,
+    entry: {
+        app: ['./index.js']
+    },
+    output: {
+        path: appRoot,
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.html$/,
+                loader: 'raw-loader'
+            },
+            {
+                test: /\.s?css$/,
+                use: ExtractTextPlugin.extract(["css-loader"]),
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: appRoot + '/index.html',
+            inject: 'body'
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        }),
+        new ExtractTextPlugin('[hash].bundle.css'),
+        new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: '[hash].vendor.js'}),
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "app"),
+        compress: true,
+        host: "0.0.0.0",
+        port: 9000
+    }
+};
