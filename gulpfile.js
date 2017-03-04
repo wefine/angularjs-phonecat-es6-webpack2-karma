d06b971556e653858da95a49183bb7430d95e0dd
wefine <wang.xiaoren@zte.com.cn>
2017-03-04 09:36:45 +0800

    add some production test
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,23 @@
 let gulp = require('gulp');
+let uglify = require('gulp-uglify');
+let concat = require('gulp-concat');
+let babel = require('gulp-babel');
+
+let gulpWebpack = require('webpack-stream');
+let webpack = require('webpack');
+
 let templateCache = require('gulp-angular-templatecache');
 
-gulp.task('default', function () {
+
+gulp.task('es6', () => {
+    return gulp.src('dist/**/*.bundle.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('template', function () {
     return gulp.src('src/app/**/*.template.html')
         .pipe(templateCache({
             filename: 'templates.module.js',
@@ -9,4 +25,10 @@ gulp.task('default', function () {
             moduleSystem: 'ES6'
         }))
         .pipe(gulp.dest('src/app'));
+});
+
+gulp.task('webpack', function () {
+    gulp.src('src/app/index.module.js')
+        .pipe(gulpWebpack(require('./webpack.config.js'), webpack))
+        .pipe(gulp.dest('dist/'));
 });
